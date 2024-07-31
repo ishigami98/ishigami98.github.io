65df2adf40cb3e0aa77a32480db12000b263bc66
Ishigami <86505971+ishigami98@users.noreply.github.com>
2024-07-31 10:27:11 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,8 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
     const menuBtn = document.querySelector('.menu-btn');
     const menu = document.querySelector('nav ul.menu');
 
-    menuBtn.addEventListener('click', function() {
+    menuBtn.addEventListener('click', function () {
         menu.classList.toggle('open');
     });
+
+    // Smooth scrolling to 'Popular Forums' (if necessary)
+    // const scrollToForumsBtn = document.querySelector('.some-button-class');
+    // scrollToForumsBtn.addEventListener('click', function (e) {
+    //     e.preventDefault();
+    //     document.querySelector('.popular-forums').scrollIntoView({
+    //         behavior: 'smooth'
+    //     });
+    // });
 });
