38f292677329a8bfb371fb8f74ec5230d515db55
Ishigami <86505971+ishigami98@users.noreply.github.com>
2024-08-08 01:03:35 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('.hero-text button');
-    button.addEventListener('click', () => {
-        window.location.href = '#';
+    const menuButton = document.getElementById('menu-button');
+    const menuPanel = document.getElementById('menu-panel');
+    const searchButton = document.getElementById('search-button');
+    const searchBar = document.getElementById('search-bar');
+
+    menuButton.addEventListener('click', () => {
+        menuPanel.classList.toggle('hidden');
+    });
+
+    searchButton.addEventListener('click', () => {
+        searchButton.classList.add('hidden');
+        searchBar.classList.remove('hidden');
     });
 });
