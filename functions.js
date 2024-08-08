1629ef451c214933d5785b35d021f44585e5b133
Ishigami <86505971+ishigami98@users.noreply.github.com>
2024-08-08 00:40:21 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,19 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuBtn = document.querySelector('.menu-btn');
-    const menu = document.querySelector('nav ul.menu');
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.getElementById('menu-button');
+    const menuPanel = document.getElementById('menu-panel');
+    const searchButton = document.getElementById('search-button');
+    const searchBar = document.getElementById('search-bar');
 
-    menuBtn.addEventListener('click', function () {
-        menu.classList.toggle('open');
+    menuButton.addEventListener('click', () => {
+        menuPanel.classList.toggle('hidden');
     });
 
-    // Scroll to 'Most Popular Forums' section
-    const scrollToForumsBtn = document.querySelector('.hero-text input');
-    scrollToForumsBtn.addEventListener('keydown', function (e) {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            document.querySelector('.popular-forums').scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
+    searchButton.addEventListener('click', () => {
+        searchButton.classList.add('hidden');
+        searchBar.classList.remove('hidden');
     });
 });
